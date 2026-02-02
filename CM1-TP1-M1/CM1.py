baa443971957feeaaf702f035eef0624a3347373
Inaki1210 <inaki1210@gmail.com>
2026-02-02 10:04:25 +0100

    Update CM1.py

--- CM1-TP1-M1/CM1.py
+++ CM1-TP1-M1/CM1.py
@@ -3,7 +3,6 @@
 #identifiant tel que print (impression), i (variable, nommée au choix)
     #variable ??? zone de mémoire placée dans le programme permettant de garder cette valeur et de la réutiliser autre part
 #comment affecter une valeur à une variable ? i = 24 par ex
-#possibilité d'exécuter thonny pas par pas. avec le symbole scarabé.
 
 i = 100
 while i >= 0:
@@ -12,4 +11,5 @@ while i >= 0:
     else:
         print("Décollage")
     i = i - 1
+
 print("Réussi")
